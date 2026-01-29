c483589334056ed0330185da5bd9e0ac38419cb9
Lucas Guedes Barboza <lucasguedesbarboza@gmail.com>
2026-01-29 22:09:24 +0000

    feat: Initialize repository with dependencies.

---
+++ src/main/java/io/github/lucasgb/transaction_scheduler_api/TransactionSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.lucasgb.transaction_scheduler_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionSchedulerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionSchedulerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lucasgb/transaction_scheduler_api/TransactionSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lucasgb.transaction_scheduler_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionSchedulerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
