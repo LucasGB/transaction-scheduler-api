486f4554d2fa41e88bdb50859576b5a9c7bed4a4
Lucas Guedes Barboza <lucasguedesbarboza@gmail.com>
2026-01-30 00:21:49 +0000

    feat: Implement TransactionSchedule domain entity.

---
+++ src/main/java/io/github/lucasgb/transaction_scheduler_api/domain/entity/TransactionSchedule.java
@@ -0,0 +1,28 @@
+package io.github.lucasgb.transaction_scheduler_api.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@Entity
+@Table(name = "transaction_schedule")
+public class TransactionSchedule {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    //    private Money money;
+    //    private Money fee;
+    //    private Money totalAmount;
+    private String sourceAccount;
+    private String targetAccount;
+    private LocalDate scheduleDate;
+    private String createdAt;
+    private String updatedAt;
+}
